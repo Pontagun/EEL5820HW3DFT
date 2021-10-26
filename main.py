fdc3cd62a2534c26034b4dfc82e3c2d9c7c5199d
Pontagun <pontagun@gmail.com>
2021-10-25 23:44:29 -0400

    Adding data set for HW

--- main.py
+++ main.py
@@ -2,16 +2,17 @@ import cv2
 import numpy as np
 from PIL import Image
 import math
+import time
 
 
 def dft(input_img, rows, cols):
     output_img = np.zeros((rows, cols), complex)
-    for m in range(0, rows):  # Moving along rows
-        for n in range(0, cols):  # Moving along cols
+    for u in range(0, rows):  # Moving along rows
+        for v in range(0, cols):  # Moving along cols
             for x in range(0, rows):  # Evaluation loop
                 for y in range(0, cols):  # Evaluation loop
-                    output_img[m][n] += input_img[x][y] * np.exp(-1j * 2 * math.pi * (m * x / rows + n * y / cols))
-            output_img[m][n] = output_img[m][n] / (rows*cols)
+                    output_img[u][v] += input_img[x][y] * np.exp(-1j * 2 * math.pi * (u * x / rows + v * y / cols))
+            output_img[u][v] = output_img[u][v] / (rows*cols)
     return output_img
 
 
@@ -37,21 +38,24 @@ def center_image(image):
 
 
 if __name__ == "__main__":
-    path = r'8x8.jpg'
+    path = r'Dataset/64x64.jpg'
     img = cv2.imread(path, 0)
     rows = img.shape[0]
     cols = img.shape[1]
 
     ori_image = img
-
+    ori_image = center_image(ori_image)
+    start_time = time.time()
     image = dft(ori_image, rows, cols)
     # image = np.fft.fft2(ori_image) # comparing to standard library
 
     image = abs(image)
     norm_image = pixel_normalization(image)
 
+    print("___ %s seconds ___" % (time.time() - start_time))
+
     im = Image.fromarray(norm_image)
 
     if im.mode != 'RGB':
         im = im.convert('RGB')
-    im.save("8x8result.jpg")
+    im.save("64x64dft.jpg")
